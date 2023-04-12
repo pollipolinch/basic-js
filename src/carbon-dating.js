425c06f51c4db6c6acffd030d2ddcdb097e860d1
pollipolinch <pollifriend@mail.ru>
2023-04-13 02:08:53 +0500

    feat: add solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,20 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (
+    typeof sampleActivity !== "string" ||
+    !isFinite(sampleActivity) ||
+    sampleActivity <= 0 ||
+    sampleActivity > MODERN_ACTIVITY
+  )
+    return false;
+
+  const RADIOACTIVE_DECAY_CONSTANT = 0.693 / HALF_LIFE_PERIOD;
+
+  return Math.ceil(
+    Math.log(MODERN_ACTIVITY / +sampleActivity) / RADIOACTIVE_DECAY_CONSTANT
+  );
 }
 
 module.exports = {
