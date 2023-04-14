53ea9cff00c789d3d6e88e1226102859e0253931
pollipolinch <pollifriend@mail.ru>
2023-04-14 17:35:10 +0500

    feat:done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function repeater(str, options) {
+  if(options.separator === undefined) {
+    options.separator = '+';
+  }
+ if(options.additionSeparator === undefined) {
+  options.additionSeparator = '|';
+ }
+ if(options.addition === undefined) {
+   options.addition = '';
+ } 
+ if(options.additionRepeatTimes === undefined) 
+   options.additionRepeatTimes = 1;
+ if(options.repeatTimes === undefined) 
+   options.repeatTimes = 1;
+  
+   let result = [];
+   let addition = [];
+ for(let j = 1; j <= options.additionRepeatTimes; j++) {
+       addition.push(`${options.addition}`); 
+     }
+  let allAddition = addition.join(`${options.additionSeparator}`)
+   console.log(allAddition)
+   
+  console.log(options.repeatTimes)
+   for(let i = 1; i <= options.repeatTimes; i++) {
+      {
+       result.push(`${str}${allAddition}`); 
+     }
+    
+     
+   }
+  console.log(result)
+   return result.join(`${options.separator}`)
+ }
 
 module.exports = {
   repeater
